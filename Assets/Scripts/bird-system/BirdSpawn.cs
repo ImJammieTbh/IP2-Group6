cebd512b2d5ea9190bd8cfec6ebf0005d6c567d5
ImJamie <jamie@929design.co.uk>
2026-02-12 00:03:22 +0000

    Bird Spawning implemented
    Spawning of "birds" ( objects ) within the custom bounding area box has been added with features such as:
     - custom bounding box to allow for later change
     - spawn side switching based off of a switch chance percentage which is tweakable
     - y spawning is set as to not allow "birds" to spawn too close to another bird on the y axis but also constrains to the size of the spawn area

--- Assets/Scripts/bird-system/BirdSpawn.cs
+++ Assets/Scripts/bird-system/BirdSpawn.cs
@@ -5,6 +5,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using Random = System.Random;
 
+[RequireComponent(typeof(BoundsBox))]
 public class BirdSpawn : MonoBehaviour
 {
     private BoundsBox _box;
@@ -26,6 +27,15 @@ public class BirdSpawn : MonoBehaviour
 
     [Tooltip("Chance from 0 - 1 that the spawn side will swap on next spawn.")]
     [Range(0f, 1f)] public float switchChance;
+    
+    [Tooltip("Minimum allowed distance between consecutive Y spawns.")]
+    public float minYDistance = 1.5f;
+
+    [Tooltip("Forced offset if spawn is too close.")]
+    public float forcedYOffset = 2f;
+
+    private bool _hasSpawnedBefore = false;
+
 
     private void Awake()
     {
@@ -58,19 +68,42 @@ public class BirdSpawn : MonoBehaviour
 
     public Vector3 GetSpawnPos()
     {
-        if (_lastSpawnPos == null)
+        //First spawn where it decides the initial side and Y value to spawn a bird
+        if (!_hasSpawnedBefore)
         {
-            _lastSpawnPos = UnityEngine.Random.Range(0f, 1f) < 0.5f ? _spawnLeft : _spawnRight;
+            _lastSpawnPos = UnityEngine.Random.value < 0.5f ? _spawnLeft : _spawnRight;
+            _lastSpawnPos.y = UnityEngine.Random.Range(_minY, _maxY);
+            _hasSpawnedBefore = true;
             return _lastSpawnPos;
         }
 
-        //chooses the side it spawns on
+        //Decide whether to switch sides on next spawn
         if (UnityEngine.Random.value < switchChance)
         {
             _lastSpawnPos.x = _lastSpawnPos.x == _spawnLeft.x ? _spawnRight.x : _spawnLeft.x;
         }
-        
-        _lastSpawnPos.y = UnityEngine.Random.Range(_minY, _maxY);
+
+        float newY = UnityEngine.Random.Range(_minY, _maxY);
+
+        //Check if the Y value of next spawn will be too close to previous Y
+        if (Mathf.Abs(newY - _lastSpawnPos.y) < minYDistance)
+        {
+            //Push new spawn of Y up or down ( 50% chance ) 
+            
+            //bool to decide if the new spawn goes up or down
+            bool goUp = UnityEngine.Random.value < 0.5f;
+
+            if (goUp)
+                newY = _lastSpawnPos.y + forcedYOffset;
+            else
+                newY = _lastSpawnPos.y - forcedYOffset;
+
+            //Clamp the values to the bounds of the spawn area
+            newY = Mathf.Clamp(newY, _minY, _maxY);
+        }
+
+        _lastSpawnPos.y = newY;
+
         return _lastSpawnPos;
     }
 }
