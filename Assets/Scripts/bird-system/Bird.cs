97ec1b615b088f70450b05310f852a90067f5544
ImJamie <jamie@929design.co.uk>
2026-02-12 14:39:23 +0000

    Adds initial bird system implementation
    Adds a basic bird system with a single bird sprite.
    This includes the bird prefab, associated scripts, and necessary assets.
    The bird sprite is flipped based on its spawn position.
    
    Adds sprite package dependency for bird rendering.

--- Assets/Scripts/bird-system/Bird.cs
+++ Assets/Scripts/bird-system/Bird.cs
@@ -1,15 +1,37 @@
 using System;
 using System.Collections;
+using System.Net.Http.Headers;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    private string _birdName;
-    private float _birdSpeed;
+    public string birdName;
+    public float birdSpeed;
+    
+    public SpriteRenderer spriteRenderer;
+
+    private bool _spawnedLeft;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Start()
     {
-        FlyTo();
+        //Swaps the orientation of the sprite based on where it spawned
+        switch (transform.position.x)
+        {
+            case < 0:
+                spriteRenderer.flipX = false;
+                _spawnedLeft = true;
+                break;
+            
+            case > 0:
+                spriteRenderer.flipX = true;
+                _spawnedLeft = false;
+                break;
+        }
     }
 
     private void FlyTo()
