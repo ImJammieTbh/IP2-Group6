74535e8e39bec5e045ca11cd4aca172d3b670172
ImJamie <jamie@929design.co.uk>
2026-02-27 16:21:54 +0000

    Added duplicate spawn protection
    Simple check when grabbing the new random bird, checks last bird then decides based on that

--- Assets/Scripts/bird-system/Scriptables/BirdSpawnTable.cs
+++ Assets/Scripts/bird-system/Scriptables/BirdSpawnTable.cs
@@ -8,6 +8,8 @@ namespace bird_system
     public class BirdSpawnTable : ScriptableObject
     {
         public List<BirdData> birds;
+        
+        public BirdData lastBird;
 
         public BirdData GetRandomBird(BirdData.Biome biome, bool isNight)
         {
@@ -16,6 +18,13 @@ namespace bird_system
             
             if (validBirds.Count == 0)
                 return null;
+
+            //remove the last spawned bird
+            //only if we have more than 1 option 
+            if (lastBird != null && validBirds.Count > 1)
+            {
+                validBirds.Remove(lastBird);
+            }
             
             float totalWeight = validBirds.Sum(b => b.spawnWeight);
             float randomValue = Random.Range(0, totalWeight);
@@ -26,9 +35,15 @@ namespace bird_system
             {
                 current += bird.spawnWeight;
                 if (randomValue <= current)
+                {
+                    lastBird = bird; //storing new last bird
                     return bird;
+                }
             }
 
+            //Fallback safety
+            //should rarely even hit it
+            lastBird = validBirds[0];
             return validBirds[0];
         }
     }
