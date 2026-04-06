6896ce97667465460bc5f27b7a64cb38637c9218
Kah-Weng-Lai <klai300@caledonian.ac.uk>
2026-04-06 16:17:15 +0100

    added sounds to UI buttons

--- Assets/Scripts/- Manager -/-- Menu --/GameMenuController.cs
+++ Assets/Scripts/- Manager -/-- Menu --/GameMenuController.cs
@@ -60,10 +60,15 @@ public class GameMenuController : MonoBehaviour
     public bool isPaused = false;
     public GameObject cameraObject; // disables the camera so it doesn't block the menu
     //stop all audio when paused, source: https://discussions.unity.com/t/how-to-stop-all-audio/32919
+
+
+    [Header("SFX")] //for clicking the buttons
     private AudioSource[] allAudioSources;
     public BirdEmitter birdSounds;
     public AudioSource radioMusic; //starts playing once unpaused
-
+    public float buttonVolume = 0.5f;
+    public AudioSource buttonSoundClips;
+    
 
 
     // Setting all assets as inactive
@@ -107,7 +112,7 @@ public class GameMenuController : MonoBehaviour
         if ((Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7)) && isPaused == false)  // pausing
         {
             //AudioListener.volume = 0; // TEMP SOLVER, mute all sounds, because if pause when a sound effect is playing it will play the whole sound
-            StopAllAudio();
+            //StopAllAudio(); 
 
             resumeButton.Select();
 
@@ -122,6 +127,11 @@ public class GameMenuController : MonoBehaviour
             pauseText.gameObject.SetActive(true); // pause text
             pauseButtons.gameObject.SetActive(true); // pause menu buttons
             menuBackground.gameObject.SetActive(true);
+
+            //Audio
+            birdSounds.quackCoolDown = 0;
+            radioMusic.volume = 0.05f; // lower the songs volume
+            //radioMusic.Play();
 
             //UI
             crosshair.gameObject.SetActive(false);
@@ -155,7 +165,7 @@ public class GameMenuController : MonoBehaviour
 
             //Audio
             birdSounds.quackCoolDown = 0;
-            radioMusic.Play();
+            radioMusic.volume = 0.25f;
 
             //UI
             crosshair.gameObject.SetActive(true);
@@ -271,6 +281,14 @@ public class GameMenuController : MonoBehaviour
 
 
         resumeButton.Select(); // selects the resume button when pressing the back button
+    }
+
+
+    //CLICK SOUND
+    public void OnClickSound()
+    {
+        buttonSoundClips.volume = buttonVolume;
+        buttonSoundClips.Play();
     }
 
 
